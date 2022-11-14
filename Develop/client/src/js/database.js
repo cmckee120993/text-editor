f9502a6cb5abc15a9ccbd39895c691101300a70e
Clay McKee <cmckee120993@gmail.com>
2022-11-14 15:48:55 -0500

    troubleshooting

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -15,7 +15,7 @@ const initdb = async () =>
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   console.log('PUT to the database');
-  const jateDb = await openDB('jate', 1);
+  const jateDB = await openDB('jate', 1);
   const jx = jateDB.transaction('jate', 'readwrite');
   const store = jx.objectStore('jate');
   const request = store.put({ id: id, jate: content });
@@ -26,7 +26,7 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   console.log('GET all from the database');
-  const jateDb = await openDB('jate', 1);
+  const jateDB = await openDB('jate', 1);
   const jx = jateDB.transaction('jate', 'readwrite');
   const store = jx.objectStore('jate');
   const request = store.getAll();
